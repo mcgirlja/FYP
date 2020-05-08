71e00be7fdb3f969b9c57b17bee71b0b7ab8957f
James Rowntree <32641023+mcgirlja@users.noreply.github.com>
2020-05-08 16:02:23 +0100

    FINAL CHANGES

--- Project code/PiBook.py
+++ Project code/PiBook.py
@@ -16,8 +16,8 @@ class pibook(object):
         self.matches = [] # used in get_matches_kaze() method as a variable to pass matches.
         self.storedImageDes = None #image descriptors paired with file path string.
         self.imagesWithAudio = {}  #Used to pair image file path string with corresponding audio path string.
-
-
+        
+    
     def capture_Image(self):
         cap = cv2.VideoCapture(0)
         if cap.isOpened():
@@ -29,21 +29,21 @@ class pibook(object):
         return img1
 
 
-    def unpickle_database(self):
-        infile = open("dict.pickle","rb")
-        self.storedImageDes = pickle.load(infile)
+    def unpickle_database(self): 
+        infile = open("dict.pickle","rb") 
+        self.storedImageDes = pickle.load(infile) #stores the precomputed descriptors.
         infile.close()
 
 
     def get_matches_Kaze(self, capturedImage):
-
+        
         for image,des2 in self.storedImageDes.items():
             print(image)
             kaze = cv2.KAZE_create() #initialise feature descriptor.
             kp1, des1 = kaze.detectAndCompute(capturedImage, None) #this finds keypoints and descriptors.
             bf = cv2.BFMatcher() #initalise brute-force feature matcher.
             matches = bf.knnMatch(des1,des2, k=2) #match features.
-
+            
             # Apply the ratio test
             good = []
             for m,n in matches:
@@ -57,8 +57,8 @@ class pibook(object):
                 else:
                     self.image = None
 
-            print("curr="+str(len(good)))
-            print("best="+str(len(self.matches)))
+            print("current book="+str(len(good))+" matches")
+            print("best so far="+str(len(self.matches))+ " matches")
 
         good = []  # resets the matches for the constant loop once a match is found.
         self.matches = [] # resets the matches for the constant loop once a match is found.
@@ -71,30 +71,33 @@ class pibook(object):
             round = 0
             match_found = None
             current_matching = None
+            start_time = time.time()
             while True:
                 round += 1
                 print("Round " + str(round))
-                captured_img = self.capture_Image()
-                match_found = self.get_matches_Kaze(captured_img)
+                captured_img = self.capture_Image()#captured image
+                match_found = self.get_matches_Kaze(captured_img) #wait for matches.
                 if(match_found != None):
                     if(current_matching == match_found):
                         counter += 1
                     else:
                         current_matching = match_found
                         counter = 0
-                if(counter >= 1): #has to find book twice in a row as a confirmation of correct book.
-                    self.read_Book(current_matching)
+                if(counter >= 1): #return the same book twice in a row.
+                    end_time = time.time()
+                    print("This took " + str(end_time - start_time) + " seconds.")
+                    self.read_Book(current_matching) #pass jpg book cover image to read book.
                     counter = 0
                     round = 0
                     break
-
+                
     def populate_dict(self):
-        pathname = '/home/pi/FYP/Files'
-        globjpg = glob.glob(pathname + '/*.jpg')
-        globwav = glob.glob(pathname + '/*.wav')
+        pathname = '/home/pi/FYP/Files' 
+        globjpg = glob.glob(pathname + '/*.jpg') #find images
+        globwav = glob.glob(pathname + '/*.wav') #find audiobooks
 
         for image in globjpg:
-            for sound in globwav:
+            for sound in globwav:   
                 if(image[:-4] == sound[:-4]):
                     self.imagesWithAudio[image] = sound #Tie the jpg with the wav in a dict if match found.
 
@@ -106,17 +109,17 @@ class pibook(object):
                 wave_obj = sa.WaveObject.from_wave_file(audio)
                 play_obj = wave_obj.play()
                 GPIO.output(11,GPIO.HIGH)
-
+                
                 while play_obj.is_playing():
                     input_value = GPIO.input(12)
-                    if input_value == False: #Detects if the button is pressed.
+                    if input_value == False: #Detects if the button is pressed. 
                         print('Button has been pressed')
                         while input_value == False: #Stops it from picking up mulitple button presses.
                             input_value = GPIO.input(12)
                         play_obj.stop() #stops the book if the button has been pressed.
                         GPIO.output(11,GPIO.LOW)
                     time.sleep(0.2)  #small buffer so that the while loop isn't checking constantly which can be intensive on the OS.
-
+                    
                 play_obj.wait_done()
             else:
                 continue
@@ -127,7 +130,9 @@ def main():
     testobj.unpickle_database()
     testobj.populate_dict()
     testobj.book_Lookup()
-
-
+        
+    
 if __name__ == "__main__":
     main()
+    
+
